fca05c950d2edcaaa91606bdfefc3e5c05e5a48a
Anshul1507 <ag1507anshul@gmail.com>
2022-02-13 15:49:48 +0530

    update sample app

--- app/src/main/java/com/example/toaster_lib_test/MainActivity.kt
+++ app/src/main/java/com/example/toaster_lib_test/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.toaster_lib_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.toaster.Toaster
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toaster = Toaster()
+        toaster.display(this, "Hello Android")
     }
 }
